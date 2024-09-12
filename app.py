3e9703b1b195690b2d91b4f9ae50b7ff81e4eca5
Kadu Oliveira <carloseduardo_al@hotmail.com>
2024-09-12 12:04:49 -0300

    'Trabalhando com herança'

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante1 = Restaurante('Praca', 'Gourmet')
 restaurante2 = Restaurante('Mexican Food', 'Mexicana')
@@ -11,9 +13,15 @@ restaurante1.receber_avaliacao('Pituca', 6)
 
 Restaurante.alternar_status(restaurante2)
 
+prato1 = Prato('Macarronada', 14.9, 'Massa em camadas a bolhonhesa')
+bebida1 = Bebida('Suco de manga', 5.0, '300ml')
+
+
 def main():
     os.system('cls')
     Restaurante.listar_restaurantes()
+    print(prato1)
+    print(bebida1)
     
 
 if __name__ == '__main__':
